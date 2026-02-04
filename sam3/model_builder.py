5b776fa479512aac3cfabc5702ed3007f3b1b5e7
tigerding <zhiyuanding01@gmail.com>
2026-02-04 12:45:08 -0500

    Deprecate pkg_resources

--- sam3/model_builder.py
+++ sam3/model_builder.py
@@ -5,7 +5,7 @@
 import os
 from typing import Optional
 
-import pkg_resources
+import importlib.resources
 import torch
 import torch.nn as nn
 from huggingface_hub import hf_hub_download
@@ -588,9 +588,7 @@ def build_sam3_image_model(
         A SAM3 image model
     """
     if bpe_path is None:
-        bpe_path = pkg_resources.resource_filename(
-            "sam3", "assets/bpe_simple_vocab_16e6.txt.gz"
-        )
+        bpe_path = str(importlib.resources.files("sam3").joinpath("assets/bpe_simple_vocab_16e6.txt.gz"))
 
     # Create visual components
     compile_mode = "default" if compile else None
@@ -677,9 +675,7 @@ def build_sam3_video_model(
         Sam3VideoInferenceWithInstanceInteractivity: The instantiated dense tracking model
     """
     if bpe_path is None:
-        bpe_path = pkg_resources.resource_filename(
-            "sam3", "assets/bpe_simple_vocab_16e6.txt.gz"
-        )
+        bpe_path = str(importlib.resources.files("sam3").joinpath("assets/bpe_simple_vocab_16e6.txt.gz"))
 
     # Build Tracker module
     tracker = build_tracker(apply_temporal_disambiguation=apply_temporal_disambiguation)
